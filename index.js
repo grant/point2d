563ea382cd9e13772225c4ac672e7ab18bb1d9bd
Grant Timmerman <granttimmerman@gmail.com>
2014-09-15 09:52:06 -0400

    Added docs

--- index.js
+++ index.js
@@ -10,14 +10,40 @@ function Point (x, y) {
   Object.freeze(this);
 }
 
+/**
+ * Returns the Euclidean distance between this point and another point
+ * @param {Point} point The other point
+ * @returns {Number} The Euclidean distance
+ */
 Point.prototype.distance = function (point) {
   return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
 };
 
+/**
+ * Returns true if the x and y coordinate of this point equals the x and y coordinate of the other point
+ * @param {Point} point The other point
+ * @returns {Boolean} If the points equal
+ */
 Point.prototype.equals = function (point) {
   return this.x === point.x && this.y == point.y;
 };
 
-// Point.
+/**
+ * Returns a new point with the coordinates added together
+ * @param {Point} point The other point
+ * @returns {Point} A new point
+ */
+Point.prototype.add = function (point) {
+  return new Point(this.x + point.x, this.y + point.y);
+};
+
+/**
+ * Returns a new point with the coordinates of this point subtracted by the other point
+ * @param {Point} point The other point
+ * @returns {Point} The new point
+ */
+Point.prototype.sub = function (point) {
+  return new Point(this.x - point.x, this.y - point.y);
+};
 
 module.exports = Point;
