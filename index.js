7b59e13bc030cc061bebe04de3643979e44d8e13
Grant Timmerman <granttimmerman@gmail.com>
2014-09-15 09:47:29 -0400

    add gitignore

--- index.js
+++ index.js
@@ -7,10 +7,17 @@
 function Point (x, y) {
   this.x = x || 0;
   this.y = y || 0;
+  Object.freeze(this);
 }
 
 Point.prototype.distance = function (point) {
   return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
 };
+
+Point.prototype.equals = function (point) {
+  return this.x === point.x && this.y == point.y;
+};
+
+// Point.
 
 module.exports = Point;
---
+++ test.js
@@ -0,0 +1,59 @@
+
+var assert = require("assert");
+var Point = require('./');
+describe('point', function () {
+  describe('constructor', function () {
+    it('should accept coordiantes', function () {
+      var point = new Point(1, 2);
+      assert.equal(point.x, 1);
+      assert.equal(point.y, 2);
+    });
+
+    it('should create unique points', function () {
+      var point1 = new Point(1, 2);
+      var point2 = new Point(3, 4);
+      assert.equal(point1.x, 1);
+      assert.equal(point1.y, 2);
+      assert.equal(point2.x, 3);
+      assert.equal(point2.y, 4);
+    });
+
+    it('should default to 0, 0', function () {
+      var point = new Point();
+      assert.equal(point.x, 0);
+      assert.equal(point.y, 0);
+    });
+  });
+
+  describe('distance', function () {
+    it('should be 0 when calculating the distance to the same point', function () {
+      assert.equal(new Point().distance(new Point()), 0);
+    });
+
+    it('should be accurate', function () {
+      assert.equal(new Point().distance(new Point(3, 4)), 5);
+    });
+  });
+
+  describe('equals', function () {
+    it('should equal itself', function () {
+      assert.equal(new Point().equals(new Point()));
+    });
+
+    it('should not equal other points', function () {
+      assert.notEqual(new Point().equals(new Point(1, 2)));
+    });
+  });
+
+  describe('add', function () {
+    it('should add correctly', function () {
+      assert(new Point(1, 2).add(new Point(3, 4)).equals(new Point(4, 6)));
+    });
+  });
+
+  describe('sub', function () {
+    it('should subtract correctly', function () {
+      assert(new Point(1, 2).sub(new Point(3, 4)).equals(new Point(-2, -2)));
+    });
+  });
+});
